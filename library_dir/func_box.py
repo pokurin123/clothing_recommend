f50cc63251e4dafeec97d56f4b0648dbbe9d6d95
Kimura Taiki <s1922074@stu.musashino-u.ac.jp>
2022-09-28 02:07:28 +0900

    change

--- library_dir/func_box.py
+++ library_dir/func_box.py
@@ -1,8 +1,12 @@
-# returnで背景取り除いたPIL.Imageを返す
+"""
+remove_back(input_path) : 入力画像パスを入れるとreturnで背景取り除いたPIL.Imageを返す
+"""
+
 from rembg import remove
 from PIL import Image, ImageDraw
 
 class self_func:
+    # 入力画像を入れると背景を取り除いたPIL.Imageをreturn
     def remove_back(self,input_path):
         input_ = Image.open(input_path)
         input_resize = input_.resize((400, 600))
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from library_dir.func_box import self_func
 
+# 自作ライブラリ
 lib = self_func()
 
 im_ = lib.remove_back("images/test2.jpg")
